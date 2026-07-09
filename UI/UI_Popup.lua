3508d81281f4dbc89dc78b2b1f1dc53aba067552
Niikk <niklas.groeber@gmx.de>
2026-07-09 20:16:37 +0200

    Popup rewrite part 1

--- UI/UI_Popup.lua
+++ UI/UI_Popup.lua
@@ -67,3 +67,69 @@ function HBN.Popup:Create()
     title:SetPoint("TOP",0,-10)
 
     frame.Title = title
+
+    ---------------------------------------------------
+    -- Spieler erzeugen
+    ---------------------------------------------------
+
+    local buff = HBN.Buffs[cell.buff]
+    local class = buff.class
+    local players = HBN.Raid:GetSortedClass(class)
+
+    for i,player in ipairs(players) do
+
+        local button = CreateFrame(
+            "Button",
+            nil,
+            frame,
+            "UIPanelButtonTemplate"
+        )
+
+        button:SetSize(140,20)
+        button:SetPoint("TOP",0,-30-((i-1)*22))
+        button:SetText(player.name)
+
+        button:SetScript("OnClick",function()
+
+            HBN.Assignments:Set(
+                cell.buff,
+                cell.class,
+                player.name
+            )
+
+            HBN.UI:Refresh()
+            frame:Hide()
+
+        end)
+
+    end
+
+    ---------------------------------------------------
+    -- Keine Zuweisung
+    ---------------------------------------------------
+
+    local clear = CreateFrame(
+        "Button",
+        nil,
+        frame,
+        "UIPanelButtonTemplate"
+    )
+
+    clear:SetSize(140,20)
+    clear:SetPoint("BOTTOM",0,10)
+    clear:SetText("Keine Zuweisung")
+
+    clear:SetScript("OnClick",function()
+
+        HBN.Assignments:Set(
+            cell.buff,
+            cell.class,
+            nil
+        )
+
+        HBN.UI:Refresh()
+        frame:Hide()
+
+    end)
+
+end
